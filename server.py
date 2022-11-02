d550bac63ee52933bf9a329f408cd5b5ec2e73fc
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-11-02 14:26:50 +0530

    dot operator removed and get method implemented . The app is working fine

--- server.py
+++ server.py
@@ -67,11 +67,11 @@ def mkfiles(path,values):
     lcpu=None
     rcpu=None
     rmem=None
-    if values.pod_size=="small":
+    if values['pod_size']=="small":
         lcpu='100m'
         rcpu='100m'
         rmem='200Mi'
-    elif values.pod_size=="medium":
+    elif values['pod_size']=="medium":
         lcpu='200m'
         rcpu='200m'
         rmem='200Mi'
@@ -80,19 +80,20 @@ def mkfiles(path,values):
         rcpu='400m'
         rmem='200Mi'
 
-    try:
-        with open(path+"/deployment.yml","wb"):
-            file.write(str(deployment.format(values.app,values.app,
-            values.image,values.cport,lcpu,rcpu,rmem)))
-        with open(path+"/hpa.yml","wb"):
-            pass
-        with open(path+"/service.yml","wb"):
-            file.write(str(deployment.format(values.app,values.app,
-            values.image,values.cport,lcpu,rcpu,rmem)))
-        return True
-    except:
-        return False
+    #try:
+    with open(path+"/deployment.yml","wb") as file:
+        file.write(str(deployment.format(values.get('app'),values.get('app'),
+        values.get('image'),values.get('cport'),lcpu,rcpu,rmem)).encode())
+    with open(path+"/hpa.yml","wb") as file:
+        file.write(str(hpa.format(values.get('app'),values.get('minimum_replica'),
+        values.get('maximum_replica'),values.get('app'),values.get('cpu_threshold'))).encode())
+    with open(path+"/service.yml","wb") as file:
+        file.write(str(svc.format(values.get('app'),values.get('service_type'),
+        values.get('cport'),values.get('cport'),values.get('protocol'))).encode())
+    return True
+    #except:
+    #    return False
 val={'app': 'mydep', 'image': 'myname','pod_size':'small','cport':80,'protocol':'tcp' ,'service_type':'nodeport','cpu_threshold': 80, 'minimum_replica': 3, 'maximum_replica': 3}
-mkdir("default/frontend")
-mkfiles("default/frontend",val)
+print(mkdir("default/frontend"))
+print(mkfiles("default/frontend",val))
 
