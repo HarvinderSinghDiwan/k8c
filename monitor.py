e47519bf455f8bc544e98b35eaa97ff4eaf6e098
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-11-17 23:17:32 +0530

    prometheus server bypassed

--- monitor.py
+++ monitor.py
@@ -1,6 +1,16 @@
 from time import sleep
-from prometheus_api_client import PrometheusConnect
+import subprocess as sp
+from time import sleep
+from time import perf_counter
+start=perf_counter()
+res=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
+sleep(1)
+res2=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
+print(int(res2) - int(res))
+print("time_taken: {}".format(perf_counter()-start))
+#print(str(res).split(" "))
 def monitor(host,port,n):
+
     prom = PrometheusConnect(url ='http://'+host+':'+port+'/', disable_ssl=True)
     res1=prom.custom_query(query="""istio_requests_total{destination_service="productpage.bookinfo.svc.cluster.local", destination_app="productpage",app="productpage"}""")[0]['value'][1]
     sleep(n)
