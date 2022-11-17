725f3949e2fb4f3b600e7a119a15ddbbee02d0bb
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-11-18 00:10:10 +0530

    conditions changed

--- monitor.py
+++ monitor.py
@@ -1,20 +1,30 @@
 import subprocess as sp
 from time import sleep
 from time import perf_counter
+import json
 
 
-def monitor(host,port,n):
+def monitor():
     start=perf_counter()
     res=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
     sleep(n)
     res2=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
-    return "Requests/second : {}".format(int(res2) - int(res))+"\n"+"time_taken: {}".format(perf_counter()-start)
-
+    print(res,res2)
+    if 'k' in res[-1]:
+        res=res[:-1]+'000'
+    if 'k' in res2[-1]:
+        res2=res2[:-1]+'000'
+    return   int(res2) - int(res)
+    #return json.dumps({"Requests/second" : "{}".format(int(res2) - int(res)),
+    #"time_taken": "{}".format(perf_counter()-start)})
 
 
 host='ec2-13-232-143-131.ap-south-1.compute.amazonaws.com'
 port='32467'
 n=1
 while True:
-    res= monitor(host,port,n)
-    print(res)
+    res= monitor()
+    if res > 20:
+        print("G")
+    else:
+        print("L")
