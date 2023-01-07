b69901835c18b9ea71c5f078ae5465b9f2d62515
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2023-01-08 02:35:55 +0530

    dynamiZed with fooncshions and perMinMon issues

--- burst.py
+++ burst.py
@@ -46,12 +46,11 @@ patchDec="""spec:
                 requests:
                     cpu: 50m
                     memory: 100Mi"""
-def findPort(port):
-    _,b=sp.getstatusoutput("kubectl get pods -n bi | grep product")
+def findPort(namespace,svc,port):
+    _,b=sp.getstatusoutput("kubectl get pods -n {} | grep {}".format(namespace,svc))
     print(len(b.split("\n")))
-    a,b=sp.getstatusoutput("kubectl get svc -n bi | grep reviews")
+    a,b=sp.getstatusoutput("kubectl get svc -n {} | grep {}".format(namespace,svc))
     res=b.split()[-2].split(',')
-    port="15090"
     count=0
     for i in res:
         if i[:len(port)] == port:
@@ -60,35 +59,33 @@ def findPort(port):
         count+=1
     return res[count].split(":")[1].split("/")[0]
 
-def perMinMon():
+def perMinMon(server,port,svc,namespace,oneDuration,twoDuration,threshold):
+    port=findPort(namespace,svc,port)
     start=perf_counter()
-    res=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
-    sleep(1)
-    res2=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
-
+    res=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
+    sleep(oneDuration)
+    res2=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
     if 'k' in res[-1]:
         res=res[:-1]+'000'
     if 'k' in res2[-1]:
         res2=res2[:-1]+'000'
     th=int(res2) - int(res)
     global switch
-    if th >=200:
-        
-        res=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
-        sleep(10)
-        res2=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
+    if th >= threshold:
+        res=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
+        sleep(twoDuration)
+        res2=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
         th2=int(res2) - int(res) 
         if th2 >= th:
             sp.getstatusoutput("kubectl patch deployment {} --patch-file patch.yml".format(depname))
             switch=True
             while switch is not False:
-                res=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
+                res=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
                 sleep(1)
-                res2=sp.getstatusoutput("curl localhost:32467/stats/prometheus | grep istio_requests_total | grep productpage.bookinfo.svc.cluster.local")[1].split()[43:44][0]
+                res2=sp.getstatusoutput("curl {}:{}/stats/prometheus | grep istio_requests_total | grep {}.{}.svc.cluster.local".format(server,port,svc,namespace))[1].split()[43:44][0]
                 th3=int(res2) - int(res)
-                if th3 <= 125:
+                if th3 <= threshold:
                     switch = False
+                    "backtoNormal"
+                    break
                 else: pass
-
-        elif res < th:
-            sp.getstatusoutput("kubectl patch deployment {} --patch-file patch2.yml".format(depname))
